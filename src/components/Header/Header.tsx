2a08b0a851e96089cff5ae99fa40ac9736605c0d
Jon-Felipe <rodriguesjf7@gmail.com>
2025-06-10 17:38:46 +0200

    header component and styles

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,40 @@
+import { FaBars } from 'react-icons/fa';
+
+// components
+import Button from '../ui/Button';
+
+// extras
+import styles from './Header.module.css';
+
+type HeaderProps = {
+  title: string;
+};
+
+function Header({ title }: HeaderProps) {
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>{title}</h1>
+
+      <div className={styles.actionsContainer}>
+        <nav className={styles.navLinks}>
+          <a href='#' className={styles.navLink}>
+            Home
+          </a>
+          <a href='#' className={styles.navLink}>
+            Jobs
+          </a>
+          <a href='#' className={styles.navLink}>
+            Categories
+          </a>
+          <a href='#' className={styles.navLink}>
+            Location
+          </a>
+        </nav>
+        <Button>Login / Sign Up</Button>
+        <FaBars className={styles.menuIcon} />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
