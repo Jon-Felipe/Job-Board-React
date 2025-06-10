87018d2da56009b31c32f312d68186bc18b85b60
Jon-Felipe <rodriguesjf7@gmail.com>
2025-06-10 18:10:31 +0200

    button component and styling updates

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,10 +3,31 @@ import React from 'react';
 // extras
 import styles from './Button.module.css';
 
-type ButtonProps = { children: React.ReactNode };
+type Variant = 'primary' | 'secondary' | 'danger';
 
-function Button({ children }: ButtonProps) {
-  return <button className={styles.button}>{children}</button>;
+type ButtonProps = {
+  children: React.ReactNode;
+  variant?: Variant;
+  disabled?: boolean;
+  isLoading?: boolean;
+};
+
+function Button({
+  children,
+  variant = 'primary',
+  disabled = false,
+  isLoading = false,
+  ...props
+}: ButtonProps) {
+  const isDisabled = isLoading || disabled;
+
+  const cssClasses = [styles.button, styles[variant]].join(' ');
+
+  return (
+    <button className={cssClasses} disabled={isDisabled} {...props}>
+      {isLoading ? 'Loading...' : children}
+    </button>
+  );
 }
 
 export default Button;
