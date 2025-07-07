dcedfb3f741bb238aac85e2c41583706ab53fe00
Jon-Felipe <rodriguesjf7@gmail.com>
2025-07-07 16:34:51 +0200

    displaying about section title and subtitle and styling

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,5 +1,12 @@
+import styles from './About.module.css';
+
 function About() {
-  return <div>About</div>;
+  return (
+    <section className={styles.about}>
+      <h4 className={styles.title}>We work to do the best</h4>
+      <h1 className={styles.subtitle}>The Only Job Seeker Centric Platform</h1>
+    </section>
+  );
 }
 
 export default About;
