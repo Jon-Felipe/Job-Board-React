b808d46ab470d8c31a1940a81c445eac3b8f8211
Jon-Felipe <rodriguesjf7@gmail.com>
2025-07-08 11:25:09 +0200

    dummy job category constants and job category type

---
+++ src/utils/constants.ts
@@ -0,0 +1,31 @@
+interface IJobCategory {
+  id: number;
+  title: string;
+}
+
+export const jobCategories: IJobCategory[] = [
+  {
+    id: 1,
+    title: 'Customer Services',
+  },
+  {
+    id: 2,
+    title: 'Project Management',
+  },
+  {
+    id: 3,
+    title: 'Development',
+  },
+  {
+    id: 4,
+    title: 'Design',
+  },
+  {
+    id: 5,
+    title: 'Marketing',
+  },
+  {
+    id: 6,
+    title: 'Accounting/Finance',
+  },
+];
