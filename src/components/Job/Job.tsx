e7bb6574db2f85bc1ec605a5317d5f8ebf084de1
Jon-Felipe <rodriguesjf7@gmail.com>
2025-07-08 11:25:22 +0200

    displaying job categories init

--- src/components/Job/Job.tsx
+++ src/components/Job/Job.tsx
@@ -1,9 +1,17 @@
+import { jobCategories } from '../../utils/constants';
 import styles from './Job.module.css';
 
 function Job() {
   return (
     <section className={styles.job}>
       <h1 className={styles.title}>Find your favourite job</h1>
+      <div className={styles.jobCategories}>
+        {jobCategories.map((jobCategory) => (
+          <article key={jobCategory.id}>
+            <h6>{jobCategory.title}</h6>
+          </article>
+        ))}
+      </div>
     </section>
   );
 }
