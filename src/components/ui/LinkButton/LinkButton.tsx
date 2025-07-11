e78c891bb25625ec3782a7047e66f6dd56dd1881
Jon-Felipe <rodriguesjf7@gmail.com>
2025-07-11 13:22:24 +0200

    link button styling

--- src/components/ui/LinkButton/LinkButton.tsx
+++ src/components/ui/LinkButton/LinkButton.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+// extras
+import styles from './LinkButton.module.css';
+
+type Variant = 'primary';
+
 type LinkButtonProps = {
   path: string;
   children: React.ReactNode;
+  variant?: Variant;
 };
 
-function LinkButton({ path, children }: LinkButtonProps) {
-  return <Link to={path}>{children}</Link>;
+function LinkButton({ path, children, variant = 'primary' }: LinkButtonProps) {
+  const cssClasses = [styles.link, styles[variant]].join(' ');
+
+  return (
+    <Link to={path} className={cssClasses}>
+      {children}
+    </Link>
+  );
 }
 
 export default LinkButton;
