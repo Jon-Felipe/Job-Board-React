c4055a7666dfb078ffc6c905364b4c7639f26eef
Jon-Felipe <rodriguesjf7@gmail.com>
2025-07-11 18:28:28 +0200

    password input field and sign in button init

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,11 +1,21 @@
 import styles from './LoginPage.module.css';
 
+// components
+import Button from '../../components/ui/Button/Button';
+
 function LoginPage() {
   return (
     <main className={styles.login}>
       <h3 className={styles.title}>Job Board</h3>
       <form className={styles.form}>
         <input type='email' name='email' id='email' placeholder='Email' />
+        <input
+          type='password'
+          name='password'
+          id='password'
+          placeholder='Password'
+        />
+        <Button variant='inverted'>Sign In</Button>
       </form>
     </main>
   );
