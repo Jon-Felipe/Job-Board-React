2ff5ce9d55422a7b80ed87c825cf5892c63c27be
Jon-Felipe <rodriguesjf7@gmail.com>
2025-07-15 19:08:50 +0200

    refactored login page inputs with input component

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -2,18 +2,26 @@ import styles from './LoginPage.module.css';
 
 // components
 import Button from '../../components/ui/Button/Button';
+import Input from '../../components/ui/Input/Input';
 
 function LoginPage() {
   return (
     <main className={styles.login}>
       <h3 className={styles.title}>Job Board</h3>
       <form className={styles.form}>
-        <input type='email' name='email' id='email' placeholder='Email' />
-        <input
+        <Input
+          type='email'
+          name='email'
+          value=''
+          placeholder='Email'
+          onChange={() => console.log('email')}
+        />
+        <Input
           type='password'
           name='password'
-          id='password'
+          value=''
           placeholder='Password'
+          onChange={() => console.log('password')}
         />
         <Button variant='inverted'>Sign In</Button>
       </form>
