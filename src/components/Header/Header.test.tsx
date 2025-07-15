dc597bec9fe81125b6453dfe9a20532c009d86fe
Jon-Felipe <rodriguesjf7@gmail.com>
2025-07-15 19:20:38 +0200

    header component unit test fix

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,9 +1,15 @@
 import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import { MemoryRouter } from 'react-router';
 import Header from './Header';
 
 describe('Header Component', () => {
   it('renders the header component', () => {
-    render(<Header title='Header Title' />);
+    render(
+      <MemoryRouter>
+        <Header title='Header Title' />
+      </MemoryRouter>
+    );
     expect(screen.getByText(/header title/i)).toBeInTheDocument();
   });
 });
