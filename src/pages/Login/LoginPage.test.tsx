5baad728bca6e2e5ec9e050cc1d7a1da206f1254
Jon-Felipe <rodriguesjf7@gmail.com>
2025-07-18 19:20:13 +0200

    added more login page unit tests

--- src/pages/Login/LoginPage.test.tsx
+++ src/pages/Login/LoginPage.test.tsx
@@ -27,4 +27,47 @@ describe('Login Page', () => {
     expect(emailInput).toHaveValue('test@test.com');
     expect(passwordInput).toHaveValue('123456');
   });
+
+  it('should render sign up inputs on sign up button click', () => {
+    render(<LoginPage />);
+
+    const signUpButton = screen.getByRole('button', { name: /sign up/i });
+
+    fireEvent.click(signUpButton);
+
+    const nameInput = screen.getByPlaceholderText(/name/i);
+    const emailInput = screen.getByPlaceholderText(/email/i);
+    const passwordInput = screen.getByPlaceholderText(/^password$/i);
+    const confirmPasswordInput =
+      screen.getByPlaceholderText(/^confirm password$/i);
+
+    expect(nameInput).toBeInTheDocument();
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(confirmPasswordInput).toBeInTheDocument();
+  });
+
+  it('should handle sign up form text input', () => {
+    render(<LoginPage />);
+
+    const signUpButton = screen.getByRole('button', { name: /sign up/i });
+
+    fireEvent.click(signUpButton);
+
+    const nameInput = screen.getByPlaceholderText(/name/i);
+    const emailInput = screen.getByPlaceholderText(/email/i);
+    const passwordInput = screen.getByPlaceholderText(/^password$/i);
+    const confirmPasswordInput =
+      screen.getByPlaceholderText(/^confirm password$/i);
+
+    fireEvent.change(nameInput, { target: { value: 'John Doe' } });
+    fireEvent.change(emailInput, { target: { value: 'john@gmail.com' } });
+    fireEvent.change(passwordInput, { target: { value: '123456' } });
+    fireEvent.change(confirmPasswordInput, { target: { value: '123456' } });
+
+    expect(nameInput).toHaveValue('John Doe');
+    expect(emailInput).toHaveValue('john@gmail.com');
+    expect(passwordInput).toHaveValue('123456');
+    expect(confirmPasswordInput).toHaveValue('123456');
+  });
 });
