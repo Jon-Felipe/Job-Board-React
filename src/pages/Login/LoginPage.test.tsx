8b380ad4bda2c2c2d40d9efc9f815efe97c1b667
Jon-Felipe <rodriguesjf7@gmail.com>
2025-07-18 18:53:14 +0200

    first unit test for login page

--- src/pages/Login/LoginPage.test.tsx
+++ src/pages/Login/LoginPage.test.tsx
@@ -1,9 +1,17 @@
 import { describe, it } from 'vitest';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import LoginPage from './LoginPage';
 
 describe('Login Page', () => {
   it('should render the login page', () => {
     render(<LoginPage />);
+
+    const emailInput = screen.getByPlaceholderText(/email/i);
+    const passwordInput = screen.getByPlaceholderText(/password/i);
+    const signInButton = screen.getByRole('button', { name: /sign up/i });
+
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(signInButton).toBeInTheDocument();
   });
 });
