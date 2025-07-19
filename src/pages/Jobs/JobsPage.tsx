8a5e9f2b74511a227f12fb13a5de6ce054922a92
Jon-Felipe <rodriguesjf7@gmail.com>
2025-07-19 17:05:48 +0200

    job page displaying list of jobs and page styling

--- src/pages/Jobs/JobsPage.tsx
+++ src/pages/Jobs/JobsPage.tsx
@@ -1,5 +1,20 @@
+// components
+import JobCard from '../../components/Job/JobCard';
+
+// extras
+import styles from './JobsPage.module.css';
+import { jobs } from '../../utils/constants';
+
 function JobsPage() {
-  return <div>JobsPage</div>;
+  return (
+    <main>
+      <section className={styles.jobs}>
+        {jobs.map((job) => (
+          <JobCard key={job.id} job={job} />
+        ))}
+      </section>
+    </main>
+  );
 }
 
 export default JobsPage;
