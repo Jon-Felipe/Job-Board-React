26ede41d97e3466c1881dcb3e46efe45439d0052
Jon-Felipe <rodriguesjf7@gmail.com>
2025-07-21 13:57:58 +0200

    dropdown component with options and styles

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,5 +1,34 @@
-function Dropdown() {
-  return <div>Dropdown</div>;
+import { FaCaretRight } from 'react-icons/fa';
+
+// extras
+import styles from './Dropdown.module.css';
+
+interface IDropdownOptions {
+  id: number;
+  text: string;
+}
+
+type DropdownProps = {
+  title: string;
+  dropdownOptions: IDropdownOptions[];
+};
+
+function Dropdown({ title, dropdownOptions }: DropdownProps) {
+  return (
+    <div>
+      <div className={styles.toggle}>
+        <h3 className={styles.toggleTitle}>{title}</h3>
+        <FaCaretRight className={styles.toggleIcon} />
+      </div>
+      <div className={styles.dropdownOptions}>
+        {dropdownOptions.map((option) => (
+          <p key={option.id} className={styles.option}>
+            {option.text}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Dropdown;
