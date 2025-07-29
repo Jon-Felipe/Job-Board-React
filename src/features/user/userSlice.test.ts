8a9ad371bb9107222e401685e6ef8113ccffb008
Jon-Felipe <rodriguesjf7@gmail.com>
2025-07-29 17:32:05 +0200

    new user slice unit test

--- src/features/user/userSlice.test.ts
+++ src/features/user/userSlice.test.ts
@@ -1,9 +1,16 @@
 import { describe, it, expect } from 'vitest';
-import reducer, { addUser } from './userSlice';
+import reducer, { addUser, updateUser } from './userSlice';
 import type { IUser } from '../../utils/types';
 
 describe('user reducer', () => {
-  const initialState: IUser = { firstName: '', lastName: '', email: '' };
+  const initialState: IUser = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    age: '',
+    address: undefined,
+  };
 
   it('should return the initial state when no action is provided', () => {
     const nextState = reducer(undefined, { type: 'unknown' });
@@ -25,5 +32,37 @@ describe('user reducer', () => {
     expect(nextState.firstName).toBe('John');
     expect(nextState.lastName).toBe('Doe');
     expect(nextState.email).toBe('john@gmail.com');
+  });
+
+  it('should handle updateUser action and update state', () => {
+    const userPayload: IUser = {
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john@gmail.com',
+      phone: '0123456789',
+      age: 22,
+      address: {
+        street: '123 Example Street',
+        city: 'Example City',
+        postalCode: '1234',
+        country: 'Example Country',
+      },
+    };
+
+    const action = updateUser(userPayload);
+
+    const nextState = reducer(initialState, action);
+
+    expect(nextState.firstName).toBe('John');
+    expect(nextState.lastName).toBe('Doe');
+    expect(nextState.email).toBe('john@gmail.com');
+    expect(nextState.phone).toBe('0123456789');
+    expect(nextState.age).toBe(22);
+    expect(nextState.address).toEqual({
+      street: '123 Example Street',
+      city: 'Example City',
+      postalCode: '1234',
+      country: 'Example Country',
+    });
   });
 });
