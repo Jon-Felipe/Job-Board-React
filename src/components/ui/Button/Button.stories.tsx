d7649660f4b4950287726e4eefb0ede7ecea48f3
Jon-Felipe <rodriguesjf7@gmail.com>
2025-08-01 19:19:37 +0200

    added new button icon story

--- src/components/ui/Button/Button.stories.tsx
+++ src/components/ui/Button/Button.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import styled from 'styled-components';
+import { FaShoppingCart, FaArrowRight, FaUserAlt } from 'react-icons/fa';
 import Button from './Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
@@ -63,6 +64,16 @@ const Container = styled.div`
   column-gap: 10px;
 `;
 
+const ChildrenContainer = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 5px;
+
+  p {
+    margin: 0;
+  }
+`;
+
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const DefaultButton: Story = {
   args: {
@@ -97,6 +108,35 @@ export const SizeButtons: Story = {
       <Button {...args} size='small' />
       <Button {...args} size='medium' />
       <Button {...args} size='large' />
+    </Container>
+  ),
+};
+
+export const IconButtons: Story = {
+  args: {
+    type: 'button',
+    children: 'Click Me',
+  },
+  render: (args) => (
+    <Container>
+      <Button {...args} variant='primary'>
+        <ChildrenContainer>
+          <FaShoppingCart />
+          <p>Buy now</p>
+        </ChildrenContainer>
+      </Button>
+      <Button {...args} variant='outlined'>
+        <ChildrenContainer>
+          <p>Choose plan</p>
+          <FaArrowRight />
+        </ChildrenContainer>
+      </Button>
+      <Button {...args} variant='text'>
+        <ChildrenContainer>
+          <FaUserAlt />
+          <p>Login</p>
+        </ChildrenContainer>
+      </Button>
     </Container>
   ),
 };
