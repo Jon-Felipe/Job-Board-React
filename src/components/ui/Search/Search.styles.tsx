daa1af6e88bb5251b595fc93a5dd7b6a5d9ea6ef
Jon-Felipe <rodriguesjf7@gmail.com>
2025-08-04 17:08:14 +0200

    search input styling update

--- src/components/ui/Search/Search.styles.tsx
+++ src/components/ui/Search/Search.styles.tsx
@@ -4,6 +4,7 @@ export const FormContainer = styled.div`
   display: flex;
   flex-direction: column;
   row-gap: 10px;
+  width: 100%;
 
   .label {
     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
