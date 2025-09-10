bd370ec72d20846fec3c70c88bcce6f9eba4729e
Jon-Felipe <rodriguesjf7@gmail.com>
2025-09-10 09:26:06 +0200

    new register user auth slice

---
+++ src/features/api/authApiSlice.ts
@@ -0,0 +1,19 @@
+import { apiSlice } from './apiSlice';
+import type { BasicUserInfo, UserRegisterDetails } from '../../utils/types';
+
+export const authApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    registerUser: builder.mutation<
+      { user: BasicUserInfo },
+      UserRegisterDetails
+    >({
+      query: (userDetails) => ({
+        url: 'api/auth/register',
+        method: 'POST',
+        body: userDetails,
+      }),
+    }),
+  }),
+});
+
+export const { useRegisterUserMutation } = authApi;
