8be447f49b824fb07a8a04b4975c1077ce6fae2d
Jon-Felipe <rodriguesjf7@gmail.com>
2025-10-15 19:19:01 +0200

    updated broken unit tests and added new tests for dropdown component

--- src/components/Dropdown/Dropdown.test.tsx
+++ src/components/Dropdown/Dropdown.test.tsx
@@ -1,4 +1,4 @@
-import { describe, it } from 'vitest';
+import { describe, it, vi } from 'vitest';
 import { render, screen, fireEvent } from '@testing-library/react';
 import Dropdown from './Dropdown';
 
@@ -13,17 +13,33 @@ const dummyDropdownOptions: IDropdownOption[] = [
 describe('Dropdown Component', () => {
   it('should render the dropdown component with dropdown options', () => {
     render(
-      <Dropdown title='Test Dropdown' dropdownOptions={dummyDropdownOptions} />
+      <Dropdown
+        title='Test Dropdown'
+        dropdownOptions={dummyDropdownOptions}
+        selectedOptions={[]}
+        setSelectedOptions={vi.fn()}
+      />
     );
 
     const dropdown = screen.getByText(/test dropdown/i);
     expect(dropdown).toBeInTheDocument();
-    expect(screen.queryAllByRole('checkbox')).toHaveLength(2);
+
+    const dropdownOptions = screen.queryAllByRole(
+      'checkbox'
+    ) as HTMLInputElement[];
+    expect(dropdownOptions).toHaveLength(2);
+    expect(dropdownOptions[0].checked).toBe(false);
+    expect(dropdownOptions[1].checked).toBe(false);
   });
 
   it('should let user to toggle open and close dropdown', () => {
     render(
-      <Dropdown title='Test Dropdown' dropdownOptions={dummyDropdownOptions} />
+      <Dropdown
+        title='Test Dropdown'
+        dropdownOptions={dummyDropdownOptions}
+        selectedOptions={[]}
+        setSelectedOptions={vi.fn()}
+      />
     );
 
     const dropdown = screen.getByText(/test dropdown/i);
@@ -38,24 +54,44 @@ describe('Dropdown Component', () => {
     expect(dropdownOptions).toHaveLength(2);
   });
 
-  it('should let user select and deselect options when checkbox is clicked', () => {
+  it('should trigger setSelectedOptions on checkbox click', () => {
+    const mockSetSelectedOptions = vi.fn();
+
     render(
-      <Dropdown title='Test Dropdown' dropdownOptions={dummyDropdownOptions} />
+      <Dropdown
+        title='Test Dropdown'
+        dropdownOptions={dummyDropdownOptions}
+        selectedOptions={[]}
+        setSelectedOptions={mockSetSelectedOptions}
+      />
     );
 
     const dropdownOptions = screen.queryAllByRole(
       'checkbox'
     ) as HTMLInputElement[];
-    expect(dropdownOptions[0].checked).toBe(false);
-    expect(dropdownOptions[1].checked).toBe(false);
 
     fireEvent.click(dropdownOptions[0]);
-    expect(dropdownOptions[0].checked).toBe(true);
+    expect(mockSetSelectedOptions).toHaveBeenCalledTimes(1);
 
     fireEvent.click(dropdownOptions[1]);
-    expect(dropdownOptions[1].checked).toBe(true);
+    expect(mockSetSelectedOptions).toHaveBeenCalledTimes(2);
+  });
 
-    fireEvent.click(dropdownOptions[0]);
-    expect(dropdownOptions[0].checked).toBe(false);
+  it('should reflect the checked state', () => {
+    render(
+      <Dropdown
+        title='Test Dropdown'
+        dropdownOptions={dummyDropdownOptions}
+        selectedOptions={['test 1']}
+        setSelectedOptions={vi.fn()}
+      />
+    );
+
+    const dropdownOptions = screen.queryAllByRole(
+      'checkbox'
+    ) as HTMLInputElement[];
+
+    expect(dropdownOptions[0].checked).toBe(true);
+    expect(dropdownOptions[1].checked).toBe(false);
   });
 });
